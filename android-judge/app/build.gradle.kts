5898069f7b6c1558e220d8a5d9f820d06dd55d25
stacewalbridge-rgb <stace.walbridge@gmail.com>
2026-09-22 13:42:44 +0100

    Judge AI Android: add android-judge/app/build.gradle.kts

---
+++ android-judge/app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.vamalio.judgeai"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vamalio.judgeai"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
