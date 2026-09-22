437b9ba3969f9185824c74dd3e5705d2256dda56
stacewalbridge-rgb <stace.walbridge@gmail.com>
2026-09-22 16:22:18 +0100

    Judge AI Android: fresh white-theme install package

--- android-judge/app/build.gradle.kts
+++ android-judge/app/build.gradle.kts
@@ -5,10 +5,10 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.vamalio.judgeai.clean20260922a"
+        applicationId = "com.vamalio.judgeai.white20260922"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 }
